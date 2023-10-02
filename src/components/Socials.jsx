3a6c85f3dd1a52332a63e0182ea0b901c788accf
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-10-02 11:36:21 -0400

    add socials component

---
+++ src/components/Socials.jsx
@@ -0,0 +1,17 @@
+function Socials() {
+  return (
+    <section className="socials">
+      <a href="#">
+        <img src="/assets/icon-facebook.svg" alt="Facebook" />
+      </a>
+      <a href="#">
+        <img src="/assets/icon-instagram.svg" alt="Instagram" />
+      </a>
+      <a href="#">
+        <img src="/assets/icon-twitter.svg" alt="Twitter" />
+      </a>
+    </section>
+  );
+}
+
+export default Socials;

6684405db62410ec576f0ad3266d8ebc04ee8fe9
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-10-02 11:36:11 -0400

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import Socials from "./Socials";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <img src="/assets/logo-light.svg" alt="" />
+      <p>
+        The Modern Art Gallery is free to all visitors and open seven days a
+        week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+      </p>
+      <Socials />
+    </footer>
+  );
+}
+
+export default Footer;
